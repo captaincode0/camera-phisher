64836d2707096014cf8bcd0e07142cae93d930d9
diegodisant <developerdiego0@gmail.com>
2020-09-30 00:25:30 -0500

    refactor: post file

--- post.php
+++ post.php
@@ -4,16 +4,22 @@ $date = date('dMYHis');
 $imageData=$_POST['cat'];
 
 if (!empty($_POST['cat'])) {
-error_log("Received" . "\r\n", 3, "Log.log");
-
+    error_log(
+        "Received" . "\r\n",
+        3,
+        "cam-post-data.log"
+    );
 }
 
-$filteredData=substr($imageData, strpos($imageData, ",")+1);
-$unencodedData=base64_decode($filteredData);
+$filteredData = substr(
+    $imageData,
+    strpos($imageData, ",")+1
+);
+
+$unencodedData = base64_decode($filteredData);
 $fp = fopen( 'cam'.$date.'.png', 'wb' );
+
 fwrite( $fp, $unencodedData);
 fclose( $fp );
 
-exit();
-?>
-
+exit(0);
